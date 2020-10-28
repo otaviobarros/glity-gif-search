d0b0f583130b15f14e884af04e8bcb2c049d0604
Otavio Barros <ceo@pharmastation.com.br>
2020-10-28 19:40:03 -0300

    feat: add usePersistedState hook

---
+++ src/presentation/hooks/usePersistedState.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState, Dispatch, SetStateAction } from 'react'
+
+type UsePersistedStateResponse<S> = [S, Dispatch<SetStateAction<S>>]
+
+export const usePersistedState = <S>(
+  key: string,
+  initialState: S
+): UsePersistedStateResponse<S> => {
+  const [state, setState] = useState(() => {
+    if (process.browser) {
+      const storageValue = localStorage.getItem(key)
+      if (storageValue) {
+        return JSON.parse(storageValue)
+      }
+    }
+    return initialState
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state))
+  }, [state])
+
+  return [state, setState]
+}

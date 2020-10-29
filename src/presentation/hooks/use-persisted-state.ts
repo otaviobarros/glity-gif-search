35081ae702538c02c73535f9b11a9cca7e7f741d
Otavio Barros <ceo@pharmastation.com.br>
2020-10-29 14:27:37 -0300

    refactor: rename usePersistedState file

--- src/presentation/hooks/use-persisted-state.ts
+++ src/presentation/hooks/use-persisted-state.ts
@@ -4,12 +4,13 @@ type UsePersistedStateResponse<S> = [S, Dispatch<SetStateAction<S>>]
 
 export const usePersistedState = <S>(
   key: string,
-  initialState: S
+  initialState: S = null
 ): UsePersistedStateResponse<S> => {
   const [state, setState] = useState(() => {
     if (process.browser) {
       const storageValue = localStorage.getItem(key)
       if (storageValue) {
+        console.log('persist', JSON.parse(storageValue))
         return JSON.parse(storageValue)
       }
     }
